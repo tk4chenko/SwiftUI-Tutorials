a492206d428e6fbefac08d87aff0db3a55abf310
Artem Tkachenko <>
2023-08-04 11:26:03 +0300

    Creating and Combining Views.

---
+++ SwiftUI Essentials/SwiftUI Essentials/SwiftUI_EssentialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_EssentialsApp.swift
+//  SwiftUI Essentials
+//
+//  Created by Artem Tkachenko on 04.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_EssentialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
