b296d555d7ae1563edb6dbbb21f59031ac7c0dbe
Artem Tkachenko <tkachhhenko@gmail.com>
2023-08-06 19:04:14 +0300

    Building Lists and Navigation.

---
+++ SwiftUI Essentials/SwiftUI Essentials/Views/LandmarkList.swift
@@ -0,0 +1,45 @@
+//
+//  LandmarkList.swift
+//  SwiftUI Essentials
+//
+//  Created by Artem Tkachenko on 04.08.2023.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    @State private var showFavoritesOnly = false
+    
+    var filteredLandmarks: [Landmark] {
+        landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+    var body: some View {
+        NavigationView {
+        
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites only")
+                }
+                
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+                
+            }
+            .listStyle(.plain)
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LdmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
